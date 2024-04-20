a83e909b860944c9bc15cebe12a536d04fb4d972
Mehmet Genç <gencaslan555@gmail.com>
2024-04-20 23:35:39 +0300

    20.04.2024

--- src/actions/product-actions.js
+++ src/actions/product-actions.js
@@ -1,19 +1,23 @@
-"use server" // 1.şart
+"use server";
+
 import { config } from "@/helpers/config";
-import { convertFormDataToJSON, transformYupErrors } from "@/helpers/form-validation";
+import {
+	convertFormDataToJSON,
+	transformYupErrors,
+} from "@/helpers/form-validation";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 const FormSchema = Yup.object({
-    title: Yup.string().required("Required"),
-    description: Yup.string().required("Required"),
-    category: Yup.string().required("Required"),
-    price: Yup.number().required("Required"),
-    image: Yup.string().required("Required"),
-    imageBaseURL: Yup.string().required("Required"),
+	id: Yup.number().required("Required"),
+	title: Yup.string().required("Required"),
+	description: Yup.string().required("Required"),
+	category: Yup.string().required("Required"),
+	price: Yup.number().typeError("Invalid price").required("Required"),
+	imageBaseUrl: Yup.string().required("Required"),
+	image: Yup.string().required("Required"),
 });
-
 //2.şart async yapılmalı
 export const createProductAction = async(prewState, formData) => { 
     //formData nın json formatına dönüştürülmesi gerekir
@@ -24,41 +28,94 @@ export const createProductAction = async(prewState, formData) => {
     //oluştueduğumuz formschema nın senkron validate methoduna field ı gönderiyoruz
     //validasyon kısmının try catch içerisinde yapılması gerekiyor yoksa hata fırlatır
     //{abortEarly: false}=> hatayı tek tek yakalamak yerine tüm formu kontrol edip hatayı toplu olarak göstermiş olacak
-    try {
-    //validation    
-    FormSchema.validateSync(fields, {abortEarly: false});
-    fields.image=fields.imageBaseURL + fields.image;
-    delete fields.imageBaseURL;//gerek kalmadı silelim
-
-    //mutation (api ye bağlanılan kısım)
-    const res=await fetch(`${config.apiURL}/products`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(fields),        
-    })
-
-    //product ın güncel bilgilerin gelmesi için revalidate işlemi yapılmalı
-    revalidatePath("/products");
-    revalidatePath("/dashboard/products");
-
+     try {
+        //Validation
+        CreateSchema.validateSync(fields, { abortEarly: false });
+        fields.image = fields.imageBaseUrl + fields.image;
+        delete fields.imageBaseUrl;
+        //Mutation
+        const res = await fetch(`${config.apiURL}/products`, {
+            method: "post",
+            body: JSON.stringify(fields),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!res.ok) {
+            throw new Error("Something went wrong");
+        }
+        // Revalidation (Cache leri temizler)
+        revalidatePath("/products");
+        revalidatePath("/dashboard/products");
+        return {
+            ok: true,
+            message: "Products was created",
+            errors: {},
+        };
     } catch (err) {
-		if (err instanceof Yup.ValidationError) {
-			return transformYupErrors(err.inner);
-		}
-
-		return {
-			message: "Something went wrong",
-			errors: null,
-		};
+        if (err instanceof Yup.ValidationError) {
+            return transformYupErrors(err.inner);
+        }
+        return {
+            ok: false,
+            message: "Something went wrong. Try again later.",
+            errors: null,
+        };
     }
-    
-    
-    //redirect try içinde olursa hata olur
+    // Redirect komutu try-catch icinde olmamalidir
+    // redirect("/dashboard/products");
+};
+export const updateProductAction = async (prvState, formData) => {
+    const fields = convertFormDataToJSON(formData);
+    try {
+        FormSchema.validateSync(fields, { abortEarly: false });
+        fields.image = fields.imageBaseUrl + fields.image;
+        delete fields.imageBaseUrl;
+        const res = await fetch(`${config.apiURL}/products/${fields.id}`, {
+            method: "put",
+            body: JSON.stringify(fields),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!res.ok) {
+            throw new Error("Something went wrong.");
+        }
+        revalidatePath("/products");
+        revalidatePath("/dashboard/products");
+        return {
+            ok: true,
+            message: "Product was updated",
+            errors: {},
+        };
+    } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+            return transformYupErrors(err.inner);
+        }
+        return {
+            ok: false,
+            message: "Something went wrong. Try again later.",
+            errors: null,
+        };
+    }
+};
+
+
+export const deleteProductAction = async (id) => {
+    try {
+        if (!id) throw new Error("Id is missing");
+        const res = await fetch(`${config.apiURL}/products/${id}`, {
+            method: "delete",
+        });
+        if (!res.ok) {
+            throw new Error("Somethong went wrong");
+        }
+        revalidatePath("/products");
+        revalidatePath("/dashboard/products");
+    } catch (err) {
+        return {
+            message: err.message,
+        };
+    }
     redirect("/dashboard/products");
-
- }
-
-//formData=> formdan gellen bilgilerin geldiği veri tipi
-
+};
--- src/helpers/form-validation.js
+++ src/helpers/form-validation.js
@@ -1,16 +1,18 @@
 //server action için kullanılacak method
 
 export const convertFormDataToJSON = (formData) => {
-    return Object.fromEntries(formData.entries());
-}
+	return Object.fromEntries(formData.entries());
+};
 
 export const transformYupErrors = (errors) => {
-    const errObject = {};
-    errors.forEach((error) => (errObject[error.path] = error.message));
-    return {
-        message:"Validation Error",
+	const errObject = {};
+	errors.forEach((error) => (errObject[error.path] = error.message));
+
+	return {
+		ok: false,
+        message: "Something went wrong.  Try again later.",
         errors: errObject
-    }
+    };
 };
 
 
